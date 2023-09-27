718ed15b95e1ada056072db2565f89010ab31f22
Dark Sentry <thedarksentry@gmail.com>
2023-09-26 18:06:38 -0700

    Added math rounding

--- planetside2_cof_reset/recalculate.js
+++ planetside2_cof_reset/recalculate.js
@@ -1,10 +1,10 @@
 var rpm, bloom, bullets
 
 function recalculate() {
-    rpm = document.querySelector("#rpm").value
-    bloom = document.querySelector("#bloom").value
-    bullets = document.querySelector("#bullets").value
+    rpm = document.querySelector("#rpm").value;
+    bloom = document.querySelector("#bloom").value;
+    bullets = document.querySelector("#bullets").value;
 
-    document.getElementById("start-reset-time").textContent = 60/rpm*1000
-    document.getElementById("full-reset-time").textContent = ((60/rpm)+(bloom*bullets/20))*1000
+    document.getElementById("start-reset-time").textContent = Math.round(60/rpm*1000);
+    document.getElementById("full-reset-time").textContent = Math.round(((60/rpm)+(bloom*bullets/20))*1000);
 }
